2a079f81d3392d9b305f68f0173add1f40783ea2
Grzesiosiej <gzgut2021@gmail.com>
2021-11-05 21:23:08 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML> 
+<head>
+
+<meta charset="utf-8" >
+
+</head>
+<body>
+
+<h1>Dynamics</h1><br />
+ <form action="blog.php">
+ <input type="submit" value="Blog">
+ </form>
+ 
+ <form action="projects.php">
+ <input type="submit" value="Projekty">
+ </form>
+ 
+ <form action="dynamics_action.php">
+ <input type="submit" value="Dynamics WebApps">
+ </form>
+
+<form action="about_us.php">
+ <input type="submit" value="About">
+ </form>
+
+</body>
+</html>
